f7c1f4270cce32284b9d7bda45ca493eeb23d27f
Darkthrone92 <xbatiste@DESKTOP-V9U3G6V>
2024-06-11 17:27:27 +0200

    ex18

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+int	ft_putchar (char c)
+{
+	write (1, &c, 1);
+	return (0);
+}
+int	main(int argc, char *argv[])
+{
+	int	a;
+	int	b;
+
+	a = 1;
+	b = 0;
+	while (a < argc)
+	{
+		while (argv[a][b])
+		{
+			ft_putchar(argv[a][b]);
+			b++;
+		}
+		ft_putchar('\n');
+		b = 0;
+		a++;
+	}
+	return (0);
+}
